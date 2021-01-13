4f4fc7bc0802e6fcdf7ae7c1c5cdda0f0cd3f970
Balakrishnan S <balakrish0009@gmail.com>
2021-01-13 20:55:08 +0530

    Base Structure

---
+++ src/com/errandrunner/hibernateutil/HibernateUtil.java
@@ -0,0 +1,52 @@
+package com.errandrunner.hibernateutil;
+
+
+import java.util.Properties;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+import com.errandrunner.models.UserModel;
+
+public class HibernateUtil {
+ private static SessionFactory sessionFactory;
+
+ public static SessionFactory getSessionFactory() {
+  if (sessionFactory == null) {
+   try {
+    Configuration configuration = new Configuration();
+
+    // Hibernate settings equivalent to hibernate.cfg.xml's properties
+    Properties settings = new Properties();
+    settings.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
+    settings.put(Environment.URL, "jdbc:mysql://localhost:3306/errand_runner?useSSL=false");
+    settings.put(Environment.USER, "root");
+    settings.put(Environment.PASS, "root");
+    settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+
+    settings.put(Environment.SHOW_SQL, "true");
+
+    settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+    settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+
+    configuration.setProperties(settings);
+    configuration.addAnnotatedClass(UserModel.class);
+
+    ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+      .applySettings(configuration.getProperties()).build();
+    System.out.println("Hibernate Java Config serviceRegistry created");
+    sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+    return sessionFactory;
+
+   } catch (Exception e) {
+	  System.out.print("here");
+    e.printStackTrace();
+   }
+  }
+  return sessionFactory;
+ }
+}
